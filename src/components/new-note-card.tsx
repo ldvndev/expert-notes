cdaba67c8fdcdd4b0fe2fb13d88a8c252041c9a0
ldvndev <ldvndev@gmail.com>
2024-03-25 22:55:10 -0300

    Validation of form data in the new notecard

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,8 +1,41 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
-import { X } from 'lucide-react'
+import { ChevronLeft, X } from 'lucide-react'
+
+import { useState } from 'react'
+
+const newNoteSchema = zod.object({
+  content: zod.string(),
+})
+
+type NewNoteSchema = zod.infer<typeof newNoteSchema>
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+
+  const { handleSubmit, register, reset } = useForm<NewNoteSchema>({
+    resolver: zodResolver(newNoteSchema),
+  })
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleShowOnboarding() {
+    setShouldShowOnboarding(true)
+  }
+
+  function handleSaveNote(data: NewNoteSchema) {
+    toast.success('Nota criada com sucesso! ')
+
+    console.log(data)
+    reset()
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col text-left bg-slate-700 p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -21,30 +54,54 @@ export function NewNoteCard() {
             <X className="w-5 h-5" />
           </Dialog.Close>
 
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-200">
-              Adicionar nota
-            </span>
-
-            <p className="text-sm leading-6 text-slate-400">
-              Comece{' '}
-              <button className="font-medium text-lime-400 hover:underline">
-                gravando uma nota
-              </button>{' '}
-              em áudio ou se preferir{' '}
-              <button className="font-medium text-lime-400 hover:underline">
-                utilize apenas texto
-              </button>
-              .
-            </p>
-          </div>
-
-          <button
-            type="button"
-            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+          <form
+            onSubmit={handleSubmit(handleSaveNote)}
+            className="flex-1 flex flex-col"
           >
-            Salvar nota
-          </button>
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              {!shouldShowOnboarding && (
+                <button
+                  onClick={handleShowOnboarding}
+                  className="absolute top-0 left-0 p-1.5 text-slate-400 hover:text-slate-100"
+                >
+                  <ChevronLeft className="w-5 h-5" />
+                </button>
+              )}
+              <span className="text-sm mt-5 font-medium text-slate-200">
+                Adicionar nota
+              </span>
+
+              {shouldShowOnboarding ? (
+                <p className="text-sm leading-6 text-slate-400">
+                  Comece{' '}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    gravando uma nota
+                  </button>{' '}
+                  em áudio ou se preferir{' '}
+                  <button
+                    onClick={handleStartEditor}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    utilize apenas texto
+                  </button>
+                  .
+                </p>
+              ) : (
+                <textarea
+                  autoFocus
+                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none  "
+                  {...register('content')}
+                />
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+            >
+              Salvar nota
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Toaster } from 'sonner'
 import { App } from './app.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster richColors />
   </React.StrictMode>,
 )
