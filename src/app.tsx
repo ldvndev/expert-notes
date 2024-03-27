1e0bd34ad8705e01239a64a10444832cadd5d0d5
ldvndev <ldvndev@gmail.com>
2024-03-26 21:57:22 -0300

    Filtered notes

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,10 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NoteCard } from './components/note-card'
 import { NewNoteCard } from './components/new-note-card'
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 interface Note {
   id: string
@@ -11,7 +15,14 @@ interface Note {
   content: string
 }
 
+const notesSchema = zod.object({
+  query: zod.string(),
+})
+
+type NotesSchema = zod.infer<typeof notesSchema>
+
 export function App() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes')
 
@@ -20,6 +31,10 @@ export function App() {
     }
 
     return []
+  })
+
+  const { handleSubmit, register, reset } = useForm<NotesSchema>({
+    resolver: zodResolver(notesSchema),
   })
 
   function onNoteCreated(content: string) {
@@ -36,15 +51,28 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function handleSearch(data: NotesSchema) {
+    setSearch(data.query)
+    reset()
+  }
+
+  const filteredNotes =
+    search !== ''
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+        )
+      : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="Expert notes" />
 
-      <form className="w-full">
+      <form onSubmit={handleSubmit(handleSearch)} className="w-full">
         <input
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500"
+          {...register('query')}
         />
       </form>
 
@@ -53,7 +81,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {notes.map((note) => {
+        {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />
         })}
       </div>
