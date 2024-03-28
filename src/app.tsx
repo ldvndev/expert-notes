8dede201730c2920ceeb5600ea6a0bb9c0cf3d8e
ldvndev <ldvndev@gmail.com>
2024-03-27 22:22:39 -0300

    Delete notes functionality

--- src/app.tsx
+++ src/app.tsx
@@ -51,6 +51,16 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   function handleSearch(data: NotesSchema) {
     setSearch(data.query)
     reset()
@@ -64,7 +74,7 @@ export function App() {
       : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="Expert notes" />
 
       <form onSubmit={handleSubmit(handleSearch)} className="w-full">
@@ -78,11 +88,13 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {filteredNotes.map((note) => {
-          return <NoteCard key={note.id} note={note} />
+          return (
+            <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+          )
         })}
       </div>
     </div>
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -105,7 +105,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-b-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute top-0 right-0 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="w-5 h-5" />
           </Dialog.Close>
@@ -159,7 +159,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
             {isRecording ? (
               <button
-                // type="button"
+                type="button"
                 onClick={handleStopRecording}
                 className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
               >
